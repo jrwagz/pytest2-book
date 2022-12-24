d25c25383c77e4871d7643cc426df6c48a1d67d3
Justin Wagner <jrwagz@gmail.com>
2022-12-23 23:02:02 -0700

    tweaked venv

--- create_venv.sh
+++ create_venv.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 pip3 install --upgrade pip
 pip3 install pytest
 
 if [[ -e "requirements.txt" ]]; then
     pip3 install -r requirements.txt
 fi
+
+pip install -e ./cards_proj
